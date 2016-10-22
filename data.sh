8166545f8f6229bd82c4be595ceb997689269f1d
Bert Spaan <bert.spaan@gmail.com>
2016-10-22 09:24:15 -0600

    new table name

--- data.sh
+++ data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-psql -h localhost -d histograph -U postgres -f query.sql -t > data/maps.csv
+psql -h localhost -d spacetime -U postgres -f query.sql -t > data/maps.csv
 cat data/maps.csv | node convert.js > data/maps.json
