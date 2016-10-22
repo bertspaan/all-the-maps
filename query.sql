e85d542343751a48ed473f8037a9bfe89d721222
Bert Spaan <bert.spaan@gmail.com>
2016-10-22 09:23:23 -0600

    simplify geometries, and less digits

--- query.sql
+++ query.sql
@@ -1,3 +1,12 @@
-SELECT id, ST_AsGeoJSON(geometry) FROM pits
-ORDER BY ST_NPoints(geometry) DESC
-LIMIT 5000;
+SELECT
+  id,
+  ST_AsGeoJSON(
+    ST_SimplifyPreserveTopology(geometry, 0.0001),
+    6
+  )
+FROM
+  objects
+ORDER BY
+  ST_NPoints(geometry) DESC
+LIMIT
+  10000;
